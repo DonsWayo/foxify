a1f4e1bc07760d3ebccc1c9aa16422b586759e87
DonsWayo <34981526+DonsWayo@users.noreply.github.com>
2022-11-01 23:06:58 +0100

    chore: add http

---
+++ packages/http/src/index.ts
@@ -0,0 +1,16 @@
+export * from "./constants";
+export * from "./errors";
+export {
+  default as Request,
+  settings as requestSettings,
+  DEFAULT_SETTINGS as REQUEST_DEFAULT_SETTINGS,
+  HeadersI as RequestHeadersI,
+  SettingsI as RequestSettingsI,
+} from "./Request";
+export {
+  default as Response,
+  settings as responseSettings,
+  DEFAULT_SETTINGS as RESPONSE_DEFAULT_SETTINGS,
+  HeadersI as ResponseHeadersI,
+  SettingsI as ResponseSettingsI,
+} from "./Response";
